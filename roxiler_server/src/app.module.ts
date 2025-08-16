e499ded6e160942ee42b766c297c491d17e896ef
Praneeth Kumar <124180295+PraneethKumar189@users.noreply.github.com>
2025-08-16 22:48:14 +0530

    server set up

--- roxiler_server/src/app.module.ts
+++ roxiler_server/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { StoreModule } from './store/store.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'roxiler',
+    entities: [],
+    synchronize: true,
+  }), AuthModule, UsersModule, StoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
